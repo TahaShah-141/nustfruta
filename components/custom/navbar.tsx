b40cd67cb058023b758acf1064e0e002c45be7f3
Taha Shah <tahashah411@gmail.com>
2024-03-21 02:25:30 +0500

    change insta link

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -76,7 +76,7 @@ export const Navbar = () => {
             <a
               target="_blank"
               rel="noreferrer"
-              href="https://www.instagram.com/tahashah327/"
+              href="https://www.instagram.com/nustfruta/"
             >
               <div className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 py-2 w-9 px-0">
                 <InstagramLogoIcon className="h-4 w-4 fill-current" />
